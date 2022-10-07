2120924f75f75141722bd209f46d0a0858adbf64
NishaKumariGupta <92909711+NishaKumariGupta@users.noreply.github.com>
2022-10-07 10:53:57 +0530

    Create problemChar.java

---
+++ problemChar.java
@@ -0,0 +1,11 @@
+
+public class problemChar {
+    public static void main (String agrs[]){
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+        System.out.println(sb.charAt(0));
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+    }
+    
+}
